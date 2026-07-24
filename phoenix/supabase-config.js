9533d840172462c19e06f6196ab8bf1d246ce9f2
d23racecontrol <82kdvs584h@privaterelay.appleid.com>
2026-07-24 11:37:04 +0000

    Phoenix: Supabase-Verbindung vorbereitet

---
+++ phoenix/supabase-config.js
@@ -0,0 +1,3 @@
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_PTP6SMdIXxF8Fe7u4pWkyA_M4-oRjbk";
+const SUPABASE_URL = "https://yezgluwugsugyqldkgyv.supabase.co";s
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
