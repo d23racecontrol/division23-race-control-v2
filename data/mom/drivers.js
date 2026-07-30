9a9b062fa83eb8e39b9b7082737ddc39301ef1e6
d23racecontrol <82kdvs584h@privaterelay.appleid.com>
2026-07-30 11:54:44 +0200

    Delete data/mom/drivers.js

--- data/mom/drivers.js
+++
@@ -1,9 +0,0 @@
-/**
- * Standard-Fahrerkader: MoM
- *
- * Diese Liste bleibt bewusst leer. Später können hier feste Ausgangsdaten
- * hinterlegt werden. Änderungen in der Oberfläche werden pro Liga getrennt
- * im Browser gespeichert.
- */
-
-export const DEFAULT_DRIVERS = Object.freeze([]);
