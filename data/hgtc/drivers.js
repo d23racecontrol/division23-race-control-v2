417670e7dd1a1a4868599e7e5672f21a25322309
d23racecontrol <82kdvs584h@privaterelay.appleid.com>
2026-07-30 12:06:23 +0200

    Create drivers.js for Heritage GT Championship
    Added a new file for standard driver roster in the Heritage GT Championship.

---
+++ data/hgtc/drivers.js
@@ -0,0 +1,8 @@
+/**
+ * Standard-Fahrerkader: Heritage GT Championship
+ *
+ * Die Fahrer werden später direkt in Race Control eingetragen
+ * und pro Liga im Browser gespeichert.
+ */
+
+export const DEFAULT_DRIVERS = Object.freeze([]);
