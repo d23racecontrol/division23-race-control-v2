7f648ca6d62d5a3114e4eae9a3a8a7d00a86006d
d23racecontrol <82kdvs584h@privaterelay.appleid.com>
2026-07-30 12:14:34 +0200

    Create races.js for Heritage GT Championship
    Added a new module for race management in the Heritage GT Championship.

---
+++ data/hgtc/races.js
@@ -0,0 +1,7 @@
+/**
+ * Operative Rennverwaltung der Heritage GT Championship.
+ *
+ * Die Rennen werden später direkt in Race Control angelegt
+ * und im Browser gespeichert.
+ */
+export const DEFAULT_RACES = Object.freeze([]);
