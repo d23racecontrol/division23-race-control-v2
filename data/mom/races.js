6228dd259e71c3c602579594943d4d9651511b07
d23racecontrol <82kdvs584h@privaterelay.appleid.com>
2026-07-30 11:55:41 +0200

    Delete data/mom/races.js

--- data/mom/races.js
+++
@@ -1,7 +0,0 @@
-/**
- * Optionale Standardrennen dieser Liga.
- *
- * Später können hier feste Saisonkalender hinterlegt werden.
- * Aktuell startet die Rennerfassung bewusst leer.
- */
-export const DEFAULT_RACES = Object.freeze([]);
